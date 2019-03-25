a5273b625fa4cb03f9829092d119b31430092420
Simon J <you@example.com>
2019-03-25 14:03:27 +0100

    initial commit

---
+++ FyraPunktEtt.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+/*
+ * I uppgift 4.1 
+ */
+public class FyraPunktEtt {
+
+	public static void main(String[] args) {
+		
+		int Minuter = Integer.parseInt(JOptionPane.showInputDialog(null, "Skriv antalet minuter du kommer prata i telefonen så kommer vi avgöra vilket abonnemang som är bäst för dig.")); 
+		
+		if(Minuter <=33)
+			JOptionPane.showMessageDialog(null, "Välj Kontant"); 
+		else if(Minuter <= 66)
+			JOptionPane.showMessageDialog(null, "Välj Normal");
+		else
+		    JOptionPane.showMessageDialog(null, "Välj Plus");
+		
+
+	}
+
+}
